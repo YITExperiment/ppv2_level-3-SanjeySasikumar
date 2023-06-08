203dea5a74eb8b38e5654d65c03757174a530a16
SanjeySasikumar <134714325+SanjeySasikumar@users.noreply.github.com>
2023-06-08 12:37:20 +0530

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,21 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/sasikumar/Desktop/Python Projects-S.Sanjey/myfirstprogram.py
+Hello, World!
+>>> 
+= RESTART: C:/Users/sasikumar/Desktop/Python Projects-S.Sanjey/myfirstprogram1.py
+Hello, World!
+What is your name? Sanjey
+Hello  Sanjey
+>>> age=12
+>>> print(age)
+12
+>>> weight=41kg
+SyntaxError: invalid decimal literal
+>>> weight=41
+>>> print(weight)
+41
+>>> height=176
+>>> print(height)
+176
--- robot_builder.py
+++ robot_builder.py
@@ -1,4 +1,4 @@
-import turtle as t 
+import turtle as t
 
 def rectangle(horizontal,vertical,color):
     t.pendown()
@@ -19,15 +19,15 @@ t.bgcolor('Dodger blue')
 
 #feet
 t.goto(-100, -150)
-rectangle(50,20,'blue')
+rectangle(50,20,'black')
 t.goto(-30,-150)
-rectangle(50,20,'blue')
+rectangle(50,20,'black')
 
 #legs
 t.goto(-25, -50)
-rectangle(15,100,'grey')
+rectangle(15,100,'seashell')
 t.goto(-55,-50)
-rectangle(-15,100,'grey')
+rectangle(-15,100,'seashell')
 
 #body
 t.goto(-90,100)
@@ -35,18 +35,18 @@ rectangle(100,150,'red')
 
 #arms
 t.goto(-150, 70)
-rectangle(60,15,'grey')
+rectangle(60,15,'seashell')
 t.goto(-150,110)
-rectangle(15,40,'grey')
+rectangle(15,40,'seashell')
 
 t.goto(10, 70)
-rectangle(60,15,'grey')
+rectangle(60,15,'seashell')
 t.goto(55,110)
-rectangle(15,40,'grey')
+rectangle(15,40,'seashell')
 
 #neck
 t.goto(-50,120)
-rectangle(15,20,'grey')
+rectangle(15,20,'seashell')
 
 #head
 t.goto(-85,170)
@@ -55,9 +55,9 @@ rectangle(80,50,'red')
 #eyes
 t.goto(-60, 160)
 rectangle(30,10,'white')
-t.goto(-60,160)
+t.goto(-55,155)
 rectangle(5,5,'black')
-t.goto(-45,155)
+t.goto(-40,155)
 rectangle(5,5,'black')
 
 #mouth
